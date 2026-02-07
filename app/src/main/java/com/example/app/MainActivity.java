f76a617bd82b49dab1f937333a458728ea1bbcfb
BenGu103 <79404940+BenGu103@users.noreply.github.com>
2026-02-07 23:51:50 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -20,8 +20,8 @@ public class MainActivity extends Activity {
         webSettings.setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new MyWebViewClient());
 
-        // REMOTE RESOURCE
-        // mWebView.loadUrl("https://example.com");
+         REMOTE RESOURCE
+         mWebView.loadUrl("https://www.google.com");
 
         // LOCAL RESOURCE
         // mWebView.loadUrl("file:///android_asset/index.html");
